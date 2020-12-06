1bde260f5dc967bce82cb0b2c64e87b9d48c7cc5
Rovine1999 <rovinewanjala99@gmail.com>
2020-12-07 00:37:11 +0300

    add function for registering the blueprint on __init__.py file

--- manage.py
+++ manage.py
@@ -1,14 +1,14 @@
 from app import create_app,db
 from flask_script import Manager,Server
-from app.models import User,Role,Review
+from app.models import Pitches,Role,User
 from flask_migrate import Migrate, MigrateCommand
 
+app = create_app('production')
 app = create_app('development')
-app = create_app('test')
 
 
 
-manager = Manager(app, db)
+manager = Manager(app)
 manager.add_command('server',Server)
 @manager.command
 def test():
@@ -19,7 +19,7 @@ def test():
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db, User = User, Role = Role )
+    return dict(app = app,db = db, User = User, Category = Category )
 
 migrate = Migrate(app,db)
 manager.add_command('db',MigrateCommand)
